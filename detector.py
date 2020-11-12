ed1566ff2c0a198bd25145f535bcf8d842a44ae4
axeloh <axel.harstad@gmail.com>
2020-11-12 21:20:53 +0100

    Detector/Descriptor Interface and feature matching

--- detector.py
+++ detector.py
@@ -1,2 +1,143 @@
+import numpy as np
+
 import cv2
+
+
+# class KeyPoint():
+# 	def __init__(self, x, y):
+# 		self.pt = ()
+
+class DetectorDescriptorInterface:
+
+	def __init__(self, des_extractor):
+		self.frame = None
+		self.detector = None
+		self.des_extractor = des_extractor
+		self.kp = None
+		self.des = None
+
+	def get_keypoints(self, frame):
+		"""
+		Takes in a frame and detects key points
+
+		:param frame: frame from which to detect key points from
+		:return: frame location of detected points, shape: (numberOfKeypoints, 2)
+		"""
+		pass
+
+	def get_descriptors(self, frame, kp):
+		"""
+		If applicable for this detector, outputs the descriptors for each detected key point.
+		Otherwise, return None
+
+		:param frame: frame from which to detect key points from
+		:param kp: key points to compute descritors frome
+		:return: descriptors of detected points, shape: (numberOfKeypoints, descriptorDimension)
+		"""
+		pass
+
+
+class HarrisDetector(DetectorDescriptorInterface):
+	def __init__(self, des_extractor=None):
+		super().__init__(des_extractor)
+
+
+class ShiTomasiDetector(DetectorDescriptorInterface):
+	def __init__(self, des_extractor=None):
+		super().__init__(des_extractor)
+
+	def get_keypoints(self, frame):
+		corners = cv2.goodFeaturesToTrack(frame,
+										  maxCorners=5000,
+										  qualityLevel=0.1,
+										  minDistance=10,
+										  blockSize=10)
+
+		kp = [cv2.KeyPoint(c[0], c[1], 1.0) for c in np.squeeze(corners)]
+
+		#kp = self.detector.detect(frame)
+		return kp
+
+	def get_descriptors(self, frame, kp):
+		if self.des_extractor is None:
+			raise NotImplementedError("No descriptor extractor specified.")
+
+		return self.des_extractor.get_descriptors(frame, kp)
+
+
+class FAST_Detector(DetectorDescriptorInterface):
+	def __init__(self, des_extractor=None):
+		super().__init__(des_extractor)
+		self.detector = cv2.FastFeatureDetector_create(threshold=10, nonmaxSuppression=True)
+
+	def get_keypoints(self, frame):
+		kp = self.detector.detect(frame)
+		return kp
+
+	def get_descriptors(self, frame, kp):
+		if self.des_extractor is None:
+			raise NotImplementedError("No descriptor extractor specified.")
+
+		return self.des_extractor.get_descriptors(frame, kp)
+
+
+class SIFT(DetectorDescriptorInterface):
+	def __init__(self, des_extractor=None):
+		super().__init__(des_extractor)
+		self.detector = cv2.SIFT_create()
+
+	def get_keypoints(self, frame):
+		kp, des = self.detector.detectAndCompute(frame, None)
+		self.des = des
+		return kp
+
+	def get_descriptors(self, frame, kp):
+		if self.des_extractor is None:
+			return self.des
+
+		return self.des_extractor.get_descriptors(frame, kp)
+
+
+class SURF(DetectorDescriptorInterface):
+	def __init__(self, des_extractor=None):
+		super().__init__(des_extractor)
+		self.detector = cv2.xfeatures2d.SURF_create()
+
+	def get_keypoints(self, frame):
+		kp, des = self.detector.detectAndCompute(frame, None)
+		self.des = des
+		return kp
+
+	def get_descriptors(self, frame, kp):
+		if self.des_extractor is None:
+			return self.des
+
+		return self.des_extractor.get_descriptors(frame, kp)
+
+
+class ORB(DetectorDescriptorInterface):
+	def __init__(self, des_extractor=None):
+		super().__init__(des_extractor)
+		self.detector = cv2.ORB_create(nfeatures=5000)
+
+	def get_keypoints(self, frame):
+		kp, des = self.detector.detectAndCompute(frame, None)
+		self.des = des
+		return kp
+
+	def get_descriptors(self, frame, kp):
+		if self.des_extractor is None:
+			return self.des
+
+		return self.des_extractor.get_descriptors(frame, kp)
+
+
+class BRIEF_Extractor(DetectorDescriptorInterface):
+	def __init__(self, des_extractor=None):
+		super().__init__(des_extractor)
+		self.des_extractor = cv2.xfeatures2d.BriefDescriptorExtractor_create()
+
+	def get_descriptors(self, frame, kp):
+		return self.des_extractor.compute(frame, kp)
+
 
--- visual_odometry.py
+++ visual_odometry.py
@@ -1,10 +1,12 @@
 import numpy as np
 import cv2
+import detector
+
 
 STAGE_FIRST_FRAME = 0
 STAGE_SECOND_FRAME = 1
 STAGE_DEFAULT_FRAME = 2
-kMinNumFeature = 1600
+kMinNumFeature = 1200
 
 lk_params = dict(winSize=(21, 21),
                 maxLevel=6,
@@ -23,7 +25,16 @@ lk_params = dict(winSize=(21, 21),
 #                      blockSize=10)
 
 
-def featureTracking(image_ref, image_cur, px_ref):
+def feature_tracking(image_ref, image_cur, px_ref):
+    """
+    Finding point correspondences between previous and current image using Lucas-Kanade Optical Flow
+
+    :param image_ref: previous image
+    :param image_cur: current images
+    :param px_ref: key points detected/tracked in previous image
+    :return: point correspondences between previous and current image
+    """
+
     kp2, st, err = cv2.calcOpticalFlowPyrLK(image_ref, image_cur, px_ref, None, **lk_params)  # shape: [k,2] [k,1] [k,1]
     st = st.reshape(st.shape[0])
 
@@ -33,6 +44,64 @@ def featureTracking(image_ref, image_cur, px_ref):
     # idxs = np.random.choice(kp1.shape[0], min(kp1.shape[0], 10000), replace=False)
     # kp1 = kp1[idxs]
     # kp2 = kp2[idxs]
+
+    print(kp1.shape)
+    print(kp2.shape)
+    print(type(kp1[0][0]))
+    return kp1, kp2
+
+
+def feature_matching(px_ref, px_cur, des_ref, des_cur):
+    """
+    Finding point correspondences between previous and current image using Brute-Force or FLANN Matcher
+
+    :param px_ref: key points detected/tracked in previous image
+    :param px_cur: key points detected/tracked in current image
+    :param des_ref: descriptors corresponding to px_ref
+    :param des_cur: descriptors corresponding to px_cur
+    :return: point correspondences between previous and current image
+    """
+
+    print('-'*30)
+    print(f'# of point in ref: {len(px_ref)}')
+    print(f'# of descs in ref: {len(des_ref)}')
+
+    print(f'# of point in cur: {len(px_cur)}')
+    print(f'# of descs in cur: {len(des_cur)}')
+
+    #bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
+    #matches = bf.knnMatch(des_ref, des_cur, k=2)
+
+    FLANN_INDEX_LSH = 6
+    index_params = dict(algorithm=FLANN_INDEX_LSH,
+                        table_number=6,  # 12
+                        key_size=12,  # 20
+                        multi_probe_level=1)  # 2
+    search_params = dict(checks=50)  # or pass empty dictionary
+
+    flann = cv2.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(des_ref, des_cur, k=2)  # k=2 to apply ratio test
+
+    # Ratio Test as per Lowe's paper
+    # good = []
+    # for m, n in matches:
+    #     if m.distance < 0.7 * n.distance:
+    #         good.append(m)
+
+    # discard bad matches, ratio test as per Lowe's paper
+    good_matches = list(filter(lambda x: x[0].distance < 0.7 * x[1].distance,
+                          matches))
+    good_matches = [good_matches[i][0] for i in range(len(good_matches))]
+    # print(good_matches)
+
+    kp1 = []
+    kp2 = []
+    for mat in good_matches:
+        kp1.append(px_ref[mat.queryIdx])
+        kp2.append(px_cur[mat.trainIdx])
+
+    kp1 = np.array(kp1)
+    kp2 = np.array(kp2)
 
     return kp1, kp2
 
@@ -47,11 +116,19 @@ class VisualOdometry:
         self.cur_t = None
         self.px_ref = None
         self.px_cur = None
+        self.des_ref = None
+        self.des_cur = None
         self.inlier_ratio = 0
         self.focal = cam.fx
         self.pp = (cam.cx, cam.cy)
         self.trueX, self.trueY, self.trueZ = 0, 0, 0
-        self.detector = cv2.FastFeatureDetector_create(threshold=10, nonmaxSuppression=True)
+        self.correspondance_method = 'matching'
+        #brief_extractor = detector.BRIEF_extractor()
+        #self.detector = detector.SIFT(brief_extractor)
+        self.detector = detector.ORB()
+        #self.detector = detector.FAST_Detector()
+        #self.detector = cv2.FastFeatureDetector_create(threshold=10, nonmaxSuppression=True)
+        #self.detector = detector.Harris()
         with open(annotations) as f:
             self.annotations = f.readlines()
 
@@ -80,22 +157,40 @@ class VisualOdometry:
         return rel_scale
 
     def process_initial_frame(self):
-        self.px_ref = self.detector.detect(self.new_frame)
+        self.px_ref = self.detector.get_keypoints(self.new_frame)
         self.px_ref = np.array([x.pt for x in self.px_ref], dtype=np.float32)
+        if self.correspondance_method == 'matching':
+            self.des_ref = self.detector.get_descriptors(self.new_frame, self.px_ref)
+
         self.frame_stage = STAGE_SECOND_FRAME
 
     def process_second_frame(self):
-        self.px_ref, self.px_cur = featureTracking(self.last_frame, self.new_frame, self.px_ref)
+        if self.correspondance_method == 'tracking':
+            self.px_ref, self.px_cur = feature_tracking(self.last_frame, self.new_frame, self.px_ref)
+        else:
+            self.px_cur = self.detector.get_keypoints(self.new_frame)
+            self.px_cur = np.array([x.pt for x in self.px_cur], dtype=np.float32)
+            self.des_cur = self.detector.get_descriptors(self.new_frame, self.px_cur)
+            self.px_ref, self.px_cur = feature_matching(self.px_ref, self.px_cur, self.des_ref, self.des_cur)
+
         E, mask = cv2.findEssentialMat(self.px_cur, self.px_ref, focal=self.focal, pp=self.pp, method=cv2.RANSAC,
                                        prob=0.999, threshold=1.0)
         self.inlier_ratio = np.sum(mask) / (len(mask)+1)
 
         _, self.cur_R, self.cur_t, mask = cv2.recoverPose(E, self.px_cur, self.px_ref, focal=self.focal, pp=self.pp, mask=mask)
-        self.frame_stage = STAGE_DEFAULT_FRAME
+
         self.px_ref = self.px_cur
+        self.des_ref = self.des_cur
+        self.frame_stage = STAGE_DEFAULT_FRAME
 
     def process_frame(self, frame_id):
-        self.px_ref, self.px_cur = featureTracking(self.last_frame, self.new_frame, self.px_ref)
+        if self.correspondance_method == 'tracking':
+            self.px_ref, self.px_cur = feature_tracking(self.last_frame, self.new_frame, self.px_ref)
+        else:
+            self.px_cur = self.detector.get_keypoints(self.new_frame)
+            self.px_cur = np.array([x.pt for x in self.px_cur], dtype=np.float32)
+            self.des_cur = self.detector.get_descriptors(self.new_frame, self.px_cur)
+            self.px_ref, self.px_cur = feature_matching(self.px_ref, self.px_cur, self.des_ref, self.des_cur)
 
         E, mask = cv2.findEssentialMat(self.px_cur, self.px_ref, focal=self.focal, pp=self.pp, method=cv2.RANSAC,
                                        prob=0.999, threshold=1.0)
@@ -110,11 +205,12 @@ class VisualOdometry:
             self.cur_t = self.cur_t + absolute_scale * self.cur_R.dot(t)
             self.cur_R = R.dot(self.cur_R)
 
-        if self.px_ref.shape[0] < kMinNumFeature: #or frame_id % 50 == 0:
-            self.px_cur = self.detector.detect(self.new_frame)
+        if self.correspondance_method == 'tracking' and self.px_ref.shape[0] < kMinNumFeature: #or frame_id % 50 == 0:
+            self.px_cur = self.detector.get_keypoints(self.new_frame)
             self.px_cur = np.array([x.pt for x in self.px_cur], dtype=np.float32)
 
         self.px_ref = self.px_cur
+        self.des_ref = self.des_cur
 
     def update(self, img, frame_id):
         assert (img.ndim == 2 and img.shape[0] == self.cam.height and img.shape[1] == self.cam.width), \
