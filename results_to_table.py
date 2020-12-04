bc1f4b6f81ec9b44a3d3295e485f7a64d38b8b5c
wquole <wgkvaale@gmail.com>
2020-12-04 20:55:35 +0100

    Add updated script

--- results_to_table.py
+++ results_to_table.py
@@ -31,14 +31,14 @@ def find_numerical_results_from_logs(filepath, outpath):
             print("\n"+"-"*30)
 
 
-def fill_in_latex_table(filepath, outpath, metric_index):
-    shi, sift, censure, akaze, orb = [], [], [], [], []
+def fill_in_latex_table_for_descriptor_based_experiments(filepath, outpath, metric_index):
+    shi, sift, fast, censure, akaze, orb = [], [], [], [], [], []
     for experiment in (sorted(glob(f"{filepath}/**"))):
         filename = experiment.split("/")[-1]
         info = filename.rstrip(".txt").split("_")
         method, detector = np.asarray(info[0:2]).ravel()
         descriptor = ""
-        if len(info) > 2: # DESCRIPTOR_BASED
+        if len(info) > 2:     # DESCRIPTOR_BASED
             descriptor = info[-1]
             print(detector, descriptor)
             with open(f"{filepath}/{filename}", 'r') as log:
@@ -56,23 +56,87 @@ def fill_in_latex_table(filepath, outpath, metric_index):
                     elif detector.lower() == "orb":
                         orb.append(res.strip())
 
-    with open(f"{outpath}/{get_metric_name(metric_index)}.txt", 'w') as out:
-        out.write("\\begin{table}[ht!]")
-        out.write("\n\\centering")
-        out.write("\n\\begin{tabular}{l|cccc}")
-        out.write("\n                                & \multicolumn{4}{c}{\\textbf{Descriptor}} \\\\")
+    with open(f"{outpath}/DB_{get_metric_name(metric_index)}.txt", 'w') as out:
+        out.write("\\begin{tabular}{l|cccc}")
+        out.write("\n\multicolumn{1}{l}{}           & \multicolumn{4}{c}{\\textbf{Descriptor}} \\\\")
         out.write("\n\cline{2-5}")
-        out.write("\n\\textbf{Detector}              & \\textit{BRIEF}    & \\textit{SIFT}  & \\textit{ORB}  & \\textit{AKAZE}      \\\\")
+        out.write("\n\\textbf{Detector}             & \\textit{BRIEF}    & \\textit{SIFT}  & \\textit{ORB}  & \\textit{AKAZE}      \\\\")
         out.write("\n\hline")
-        out.write("\n\\textit{SIFT}         " + f"   & {sift[0]}          & {sift[1]}       & -              & -                 " + "\\\\")
-        out.write("\n\\textit{Shi-Tomasi}   " + f"   & {shi[0]}           & {shi[2]}        & {shi[1]}       & -                 " + "\\\\")
-        out.write("\n\\textit{CenSurE}      " + f"   & {censure[0]}       & {censure[2]}    & {censure[1]}   & -                 " + "\\\\")
-        out.write("\n\\textit{ORB}          " + f"   & {orb[0]}           & {orb[2]}        & {orb[1]}       & -                 " + "\\\\")
-        out.write("\n\\textit{AKAZE}        " + f"   & {akaze[1]}          & {akaze[3]}     & {akaze[2]}     & {akaze[0]}       " + "\\\\")
-
+        out.write("\n\\textit{SIFT}         " + f"  & {sift[0]}          & {sift[1]}       & -              & -                 " + "\\\\")
+        out.write("\n\\textit{Shi-Tomasi}   " + f"  & {shi[0]}           & {shi[2]}        & {shi[1]}       & -                 " + "\\\\")
+        out.write("\n\\textit{CenSurE}      " + f"  & {censure[0]}       & {censure[2]}    & {censure[1]}   & -                 " + "\\\\")
+        out.write("\n\\textit{ORB}          " + f"  & {orb[0]}           & {orb[2]}        & {orb[1]}       & -                 " + "\\\\")
+        out.write("\n\\textit{AKAZE}        " + f"  & {akaze[1]}         & {akaze[3]}      & {akaze[2]}     & {akaze[0]}       " + "\\\\")
         out.write("\n\end{tabular}")
-        out.write("\n\end{table}")
 
+
+def fill_in_latex_table_for_appearance_based_experiments(filepath, outpath):
+    shi, sift, fast, censure, akaze, orb = [], [], [], [], [], []
+    for experiment in (sorted(glob(f"{filepath}/**"))):
+        filename = experiment.split("/")[-1]
+        info = filename.rstrip(".txt").split("_")
+        method, detector = np.asarray(info[0:2]).ravel()
+        if len(info) == 2:     # APPEARANCE_BASED
+            with open(f"{filepath}/{filename}", 'r') as log:
+                for lines in log.readlines()[1:7]:
+                    res = lines.split(":\t")[-1]
+                    if detector.lower() == "shi":
+                        shi.append(res.strip())
+                    elif detector.lower() == "sift":
+                        sift.append(res.strip())
+                    elif detector.lower() == "censure":
+                        censure.append(res.strip())
+                    elif detector.lower() == "akaze":
+                        akaze.append(res.strip())
+                    elif detector.lower() == "orb":
+                        orb.append(res.strip())
+
+    with open(f"{outpath}/AB_ALL.txt", 'w') as out:
+        out.write("\\begin{tabular}{l|cccccc}")
+        out.write("\n\multicolumn{1}{l}{}           & \multicolumn{6}{c}{\\textbf{Metric}} \\\\")
+        out.write("\n\cline{2-7}")
+        out.write("\n\\textbf{Detector}             & \\textit{ATE} & \\textit{ARE} & \\textit{RTE} & \\textit{RRE} &  \\texit{RIR} & \\texit{Runtime}  \\\\")
+        out.write("\n\hline")
+        out.write("\n\\textit{SIFT}         " + f"  & {sift[0]}     & {sift[1]}     & {sift[2]}     & {sift[3]}     & {sift[4]}     & {sift[5]}     " + "\\\\")
+        out.write("\n\\textit{Shi-Tomasi}   " + f"  & {shi[0]}      & {shi[1]}      & {shi[2]}      & {shi[3]}      & {shi[4]}      & {shi[5]}      " + "\\\\")
+        out.write("\n\\textit{CenSurE}      " + f"  & {censure[0]}  & {censure[1]}  & {censure[2]}  & {censure[3]}  & {censure[4]}  & {censure[5]}  " + "\\\\")
+        out.write("\n\\textit{ORB}          " + f"  & {orb[0]}      & {orb[1]}      & {orb[2]}      & {orb[3]}      & {orb[4]}      & {orb[5]}      " + "\\\\")
+        out.write("\n\\textit{AKAZE}        " + f"  & {akaze[0]}    & {akaze[1]}    & {akaze[2]}    & {akaze[3]}    & {akaze[4]}    & {akaze[5]}    " + "\\\\")
+        out.write("\n\end{tabular}")
+
+
+def fill_in_latex_two_columns_for_appearance_based_experiments(filepath, outpath, metric_index):
+    shi, sift, fast, censure, akaze, orb = [], [], [], [], [], []
+    for experiment in (sorted(glob(f"{filepath}/**"))):
+        filename = experiment.split("/")[-1]
+        info = filename.rstrip(".txt").split("_")
+        method, detector = np.asarray(info[0:2]).ravel()
+        if len(info) == 2:     # APPEARANCE_BASED
+            with open(f"{filepath}/{filename}", 'r') as log:
+                for lines in log.readlines()[metric_index+1:metric_index+2]:
+                    res = lines.split(":\t")[-1]
+                    if detector.lower() == "shi":
+                        shi.append(res.strip())
+                    elif detector.lower() == "sift":
+                        sift.append(res.strip())
+                    elif detector.lower() == "censure":
+                        censure.append(res.strip())
+                    elif detector.lower() == "akaze":
+                        akaze.append(res.strip())
+                    elif detector.lower() == "orb":
+                        orb.append(res.strip())
+
+    with open(f"{outpath}/AB_{get_metric_name(metric_index)}.txt", 'w') as out:
+        out.write("\\begin{tabular}{|l|c}")
+        out.write("\n\\multicolumn{1}{l}{} & \\\\")
+        out.write("\n\\multicolumn{1}{l}{\\textbf{Detector}} & \\\\")
+        out.write("\n\hline")
+        out.write("\n\\textit{SIFT}         " + f"  & {sift[0]}     " + "\\\\")
+        out.write("\n\\textit{Shi-Tomasi}   " + f"  & {shi[0]}      " + "\\\\")
+        out.write("\n\\textit{CenSurE}      " + f"  & {censure[0]}  " + "\\\\")
+        out.write("\n\\textit{ORB}          " + f"  & {orb[0]}      " + "\\\\")
+        out.write("\n\\textit{AKAZE}        " + f"  & {akaze[0]}    " + "\\\\")
+        out.write("\n\end{tabular}")
 
 if __name__ == '__main__':
     version_string  = f'v{argv[1]}'
@@ -85,7 +149,11 @@ if __name__ == '__main__':
     utils.create_dir(values_path)
     utils.create_dir(tables_path)
 
-    fill_in_latex_table(values_path, tables_path, int(metric_index))
+    # fill_in_latex_table_for_descriptor_based_experiments(values_path, tables_path, int(metric_index))
+    # fill_in_latex_table_for_appearance_based_experiments(values_path, tables_path)
+    fill_in_latex_two_columns_for_appearance_based_experiments(values_path, tables_path, int(metric_index))
+
+
 
 
 
